75f8eac5f3cf159c89987e580fd22330bfb631b9
Tim King <taking@cs.nyu.edu>
2013-01-23 21:52:31 -0500

    Adding improved argument handling to recentJobs.

--- scripts/recentJobs.py
+++ scripts/recentJobs.py
@@ -4,6 +4,8 @@ import MySQLdb as mdb
 import sys
 import string
 import datetime
+import argparse
+
 
 USER_FILE="../config/user"
 PASSWORD_FILE="../config/password"
@@ -38,18 +40,61 @@ def printJob(j):
     timestamp = j[7]
     print job_id, name, ";", description
     print "  ", arguments
-    print "  ", timestamp.isoformat(), problem_set_id, time_limit, problem_set_id
+    print "  ", timestamp.isoformat(), problem_set_id,
+    print time_limit, memory_limit
+
+def daysBeforeString(day_count):
+    today = datetime.date.today()
+    day_count = datetime.timedelta(days=day_count)
+    yesterday = today - day_count
+    return yesterday.strftime("%Y-%m-%d")
+
+def hoursBeforeString(h_count):
+    today = datetime.date.today()
+    hour_count = datetime.timedelta(hours=h_count)
+    diff = today - hour_count
+    return diff.strftime("%Y-%m-%d %H:%M")
+
+def selectJobsBefore(s):
+    cur.execute('SELECT * from Jobs where Jobs.timestamp >= %s;', s)
+    return cur.fetchall()
+
+def selectKMostRecent(k):
+    cur.execute('SELECT * from Jobs order by Jobs.timestamp DESC limit %s;', k)
+    return cur.fetchall()
+
+parser = argparse.ArgumentParser(description='Summarize recent benchmarking jobs.')
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-l', '--list',type=int,metavar='N',
+                   help='number of recent jobs', default=0)
+group.add_argument('-d', '--days',type=int,metavar='D',
+                   help='number of days', default=1)
+group.add_argument('-H', '--hours',type=int,metavar='H',
+                   help='hours since time', default=0)
+group.add_argument('-s', '--since',type=str,metavar='TIME',
+                   help='search since time', default="")
+args = parser.parse_args()
+
+listN = args.list
+hoursH = args.hours
+daysD = args.days
+sinceTime = args.since
+
 
 con = mdb.connect(server, user, password, table);
 with con:
-    today = datetime.date.today()
-    one_day = datetime.timedelta(days=1)
-    yesterday = today - one_day
-    yesterdayStr = yesterday.strftime("%Y-%m-%d")
-
     cur = con.cursor()
-    cur.execute('SELECT * from Jobs where Jobs.timestamp >= %s;', yesterdayStr)
-    jobs = cur.fetchall()
+    jobs = None
+    if listN > 0 :
+        jobs = selectKMostRecent(listN)
+    else:
+        if len(sinceTime) > 0:
+            prev = sinceTime
+        elif hoursH > 0:
+            prev = hoursBeforeString(hoursH)
+        else:
+            prev = daysBeforeString(daysD)
+        jobs = selectJobsBefore(prev)
     for j in jobs:
         printJob(j)
 
